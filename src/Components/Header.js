270f85a5758ffd7d2e9949da33cd3f36ef89a13a
이창우 <cwadven@naver.com>
2021-07-16 02:02:32 +0900

    feat: React Router, Components
    - export default () => something;
    - HashRouter as Router
    - BrowserRouter as Router
    - Route
    - Redirect
    - Switch

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// eslint-disable-next-line react/display-name
+export default () => (
+    <header>
+        <ul>
+            <li>
+                <a href={'/'}>Movies</a>
+            </li>
+            <li>
+                <a href={'/tv'}>TV</a>
+            </li>
+            <li>
+                <a href={'/search'}>Search</a>
+            </li>
+        </ul>
+    </header>
+);
