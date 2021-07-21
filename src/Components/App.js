b9344ee9eee223bcf773f4ce24527db1c2f91b26
이창우 <cwadven@naver.com>
2021-07-21 23:51:09 +0900

    feat: GlobalStyle & Header Design
    - Global Styled

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Router from './Router';
-import Header from './Header';
+import GlobalStyles from './GlobalStyles';
 
 const App = () => {
     return (
         <>
             <Router />
+            <GlobalStyles />
         </>
     );
 };
