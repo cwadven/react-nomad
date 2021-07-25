a5aecb1516b1782211a09912ffb0e2c4e36652c9
이창우 <cwadven@naver.com>
2021-07-25 10:29:05 +0900

    feat: HomeContainer request api
    - HomeContainer request api with await

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import HomePresenter from './HomePresenter';
+import { movieApi } from '../../api';
 
 const HomeContainer = () => {
     const [now_playing_set, setNowPlayingSet] = useState(null);
@@ -8,6 +9,28 @@ const HomeContainer = () => {
 
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    useEffect(async () => {
+        try {
+            const {
+                data: { results: results_now_playing_set },
+            } = await movieApi.nowPlaying();
+            const {
+                data: { results: results_upcoming_set },
+            } = await movieApi.upComing();
+            const {
+                data: { results: results_popular_set },
+            } = await movieApi.popular();
+
+            setNowPlayingSet(results_now_playing_set);
+            setUpcomingSet(results_upcoming_set);
+            setPopularSet(results_popular_set);
+        } catch (err) {
+            setError("Can't find movies informaiton.");
+        } finally {
+            setLoading(false);
+        }
+    }, []);
 
     return (
         <HomePresenter
