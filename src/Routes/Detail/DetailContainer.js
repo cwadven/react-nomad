7e26d50f037e921c7228bc2ff75bfa51ffaed1d6
이창우 <cwadven@naver.com>
2021-07-25 20:59:15 +0900

    feat: Route props.history & props.match
    - Route props.history & props.match used to find specific id

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DetailPresenter from './DetailPresenter';
 
-const DetailContainer = () => {
+const DetailContainer = props => {
     const [result, setResult] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const {
+            match: {
+                params: { id },
+            },
+            history: { push },
+        } = props;
+        const parsedId = Number(id);
+        if (isNaN(parsedId)) {
+            // return 을 하지 않을 경우 밑에 있는 코드까지 실행 된다.
+            return push('/');
+        }
+    }, []);
 
     return <DetailPresenter result={result} error={error} loading={loading} />;
 };
