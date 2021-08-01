0f11e71f5672f39952661f270916b79a781214f0
이창우 <cwadven@naver.com>
2021-08-01 23:12:07 +0900

    feat: Poster style setting
    - hover setting
    - Image setting
    - import Image

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,23 +3,67 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+    font-size: 12px;
+`;
 
-const ImageContainer = styled.div``;
+const Image = styled.div`
+    background-image: url(${props => props.bgUrl});
+    background-size: cover;
+    border-radius: 4px;
+    background-position: center center;
+    font-size: 12px;
+    height: 180px;
+    transition: opacity 0.1s linear;
+`;
 
-const Image = styled.div``;
+const Rating = styled.span`
+    position: absolute;
+    bottom: 5px;
+    right: 5px;
+    opacity: 0;
+    transition: opacity 0.1s linear;
+`;
 
-const Rating = styled.span``;
+// 다른 태그도 접근할 수 있다.
+const ImageContainer = styled.div`
+    position: relative;
+    margin-bottom: 5px;
+    &:hover {
+        ${Image} {
+            opacity: 0.4;
+        }
+        ${Rating} {
+            opacity: 1;
+        }
+    }
+`;
 
-const Title = styled.span``;
+const Title = styled.span`
+    display: block;
+    font-size: 12px;
+    margin-bottom: 3px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+`;
 
-const Year = styled.span``;
+const Year = styled.span`
+    font-size: 10px;
+    color: rgba(255, 255, 255, 0.5);
+`;
 
 const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
     <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
         <Container>
             <ImageContainer>
-                <Image bgUrl={imageUrl} />
+                <Image
+                    bgUrl={
+                        imageUrl
+                            ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+                            : require('../assets/no_image.png').default
+                    }
+                />
                 <Rating>
                     <span role="img" aria-label="rating">
                         ⭐
