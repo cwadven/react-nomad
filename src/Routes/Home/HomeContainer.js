cb53a673fa31cfa53f1b7b6a4645820c96e83bcd
이창우 <cwadven@naver.com>
2021-08-01 12:44:56 +0900

    feat: Section Component, Setting Section
    - Section Component added & setting with Section at HomePresenter

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -3,9 +3,9 @@ import HomePresenter from './HomePresenter';
 import { movieApi } from '../../api';
 
 const HomeContainer = () => {
-    const [now_playing_set, setNowPlayingSet] = useState(null);
-    const [upcoming_set, setUpcomingSet] = useState(null);
-    const [popular_set, setPopularSet] = useState(null);
+    const [nowPlaying, setNowPlaying] = useState(null);
+    const [upcoming, setUpcoming] = useState(null);
+    const [popular, setPopular] = useState(null);
 
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -22,9 +22,9 @@ const HomeContainer = () => {
                 data: { results: results_popular_set },
             } = await movieApi.popular();
 
-            setNowPlayingSet(results_now_playing_set);
-            setUpcomingSet(results_upcoming_set);
-            setPopularSet(results_popular_set);
+            setNowPlaying(results_now_playing_set);
+            setUpcoming(results_upcoming_set);
+            setPopular(results_popular_set);
         } catch (err) {
             setError("Can't find movies informaiton.");
         } finally {
@@ -34,9 +34,9 @@ const HomeContainer = () => {
 
     return (
         <HomePresenter
-            now_playing_set={now_playing_set}
-            upcoming_set={upcoming_set}
-            popular_set={popular_set}
+            nowPlaying={nowPlaying}
+            upcoming={upcoming}
+            popular={popular}
             error={error}
             loading={loading}
         />
