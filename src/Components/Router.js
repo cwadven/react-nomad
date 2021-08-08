c14637c432a557e83799563c234356c868fc35ac
이창우 <cwadven@naver.com>
2021-08-08 19:49:07 +0900

    feat: Season Page added
    - Season Page added

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -9,8 +9,9 @@ import Home from '../Routes/Home';
 import TV from '../Routes/TV';
 import Search from '../Routes/Search';
 import Detail from '../Routes/Detail';
-import Header from './Header';
 import Collection from '../Routes/Collection';
+import Season from '../Routes/Season';
+import Header from './Header';
 
 // eslint-disable-next-line react/display-name
 export default () => (
@@ -22,6 +23,7 @@ export default () => (
             <Route path={'/tv/popular'} render={() => <h1>Popular</h1>} />
             <Route path={'/search'} exact component={Search} />
             <Route path={'/movie/:id'} component={Detail} />
+            <Route path={'/show/:id/season/:seasonNumber'} component={Season} />
             <Route path={'/show/:id'} component={Detail} />
             <Route path={'/collection/:id'} component={Collection} />
             <Redirect from={'*'} to={'/'} />
