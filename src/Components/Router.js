a9d8a876e7ef34066c7a6312410cd18e916f3486
이창우 <cwadven@naver.com>
2021-08-10 00:00:15 +0900

    feat: github deploy
    - github deply setting

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -18,14 +18,43 @@ export default () => (
     <Router>
         <Header />
         <Switch>
-            <Route path={'/'} exact component={Home} />
-            <Route path={'/tv'} exact component={TV} />
-            <Route path={'/tv/popular'} render={() => <h1>Popular</h1>} />
-            <Route path={'/search'} exact component={Search} />
-            <Route path={'/movie/:id'} component={Detail} />
-            <Route path={'/show/:id/season/:seasonNumber'} component={Season} />
-            <Route path={'/show/:id'} component={Detail} />
-            <Route path={'/collection/:id'} component={Collection} />
+            <Route
+                path={'https://nifty-lamarr-babd35.netlify.app/'}
+                exact
+                component={Home}
+            />
+            <Route
+                path={'https://nifty-lamarr-babd35.netlify.app/tv'}
+                exact
+                component={TV}
+            />
+            <Route
+                path={'https://nifty-lamarr-babd35.netlify.app/tv/popular'}
+                render={() => <h1>Popular</h1>}
+            />
+            <Route
+                path={'https://nifty-lamarr-babd35.netlify.app/search'}
+                exact
+                component={Search}
+            />
+            <Route
+                path={'https://nifty-lamarr-babd35.netlify.app/movie/:id'}
+                component={Detail}
+            />
+            <Route
+                path={
+                    'https://nifty-lamarr-babd35.netlify.app/show/:id/season/:seasonNumber'
+                }
+                component={Season}
+            />
+            <Route
+                path={'https://nifty-lamarr-babd35.netlify.app/show/:id'}
+                component={Detail}
+            />
+            <Route
+                path={'https://nifty-lamarr-babd35.netlify.app/collection/:id'}
+                component={Collection}
+            />
             <Redirect from={'*'} to={'/'} />
         </Switch>
     </Router>
